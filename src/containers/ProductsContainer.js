fd13f40c84d722740585c3f8457f6274eacb0c5d
MACorman <madelinecorman@gmail.com>
2020-02-06 13:00:07 -0500

    did some traversing through the data to display seller on product card. added a button

--- src/containers/ProductsContainer.js
+++ src/containers/ProductsContainer.js
@@ -4,19 +4,25 @@ import ProductCard from '../components/ProductCard'
 class ProductsContainer extends React.Component {
 
     state = {
-        products: []
+        products: [],
+        users: []
     }
     componentDidMount() {
         fetch("http://localhost:3000/products")
         .then(resp => resp.json())
         .then(data => this.setState({products: data}))
+
+        fetch("http://localhost:3000/users")
+        .then(resp => resp.json())
+        .then(data => this.setState({users: data}))
     }
 
     render() {
         console.log(this.state.products)
+        console.log(this.state.users)
         return(
             <div>
-                {this.state.products.map(product => <ProductCard key={product.id} {...product} />)}
+                {this.state.products.map(product => <ProductCard key={product.id} {...product} users={this.state.users} />)}
                 
 
             </div>
